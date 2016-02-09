89280b5f41aecbd14c6f29e5b41e8756ea2b76c7
nckmrrtt <nicholasjmerritt@gmail.com>
2016-02-09 05:36:08 -0600

    archive

--- game/hello.py
+++
@@ -1,8 +0,0 @@
-import pyjd # this is dummy in pyjs
-from pyjamas.ui.RootPanel import RootPanel
-from pyjamas.ui.Label import Label
-
-pyjd.setup('public/hello.html')
-l = Label('Hello pyjs')
-RootPanel().add(l)
-pyjd.run()
--- spearmint/hi.py
+++
@@ -1,6 +0,0 @@
-from browser import document as doc
-from browser import alert
-
-def echo(*args):
-    alert("Hello %s !" %doc["zone"].value)
-
