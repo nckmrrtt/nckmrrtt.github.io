15cf8d99cfe70a96c82885de118ad93bf4228eb4
nckmrrtt <nicholasjmerritt@gmail.com>
2016-07-02 11:18:55 -0500

    hide secrets

---
+++ projects/haikubot/local_settings_example.py
@@ -0,0 +1,12 @@
+'''
+Local Settings. Don't be stupid and commit your actual info.
+'''
+
+MY_CONSUMER_KEY = ""
+MY_CONSUMER_SECRET = ""
+MY_ACCESS_TOKEN_KEY = ""
+MY_ACCESS_TOKEN_SECRET = ""
+
+TWEET_ACCOUNT = ""
+
+DEBUG = False
