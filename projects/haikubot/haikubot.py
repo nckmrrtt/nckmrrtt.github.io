dec4ec96e08655f5b12a9c75d0b30ad7ed4b50e3
nckmrrtt <nicholasjmerritt@gmail.com>
2016-07-02 02:02:50 -0500

    woot, haikubot is coming along

---
+++ projects/haikubot/haikubot.py
@@ -0,0 +1,24 @@
+import twitter
+from local_settings import *
+
+def connect():
+    api = twitter.Api(consumer_key=MY_CONSUMER_KEY,
+                      consumer_secret=MY_CONSUMER_SECRET,
+                      access_token_key=MY_ACCESS_TOKEN_KEY,
+                      access_token_secret=MY_ACCESS_TOKEN_SECRET)
+    return api
+    
+def create_tweet():
+    return "omg my first automated tweet! #myFirstTweet #camelCase"
+
+                                                    
+if __name__=="__main__":
+    
+    api = connect()
+    tweet = create_tweet()
+    print "posting tweet: " + tweet
+
+    if DEBUG==False:
+        status = api.PostUpdate(tweet)
+    else:
+        print "jk"
